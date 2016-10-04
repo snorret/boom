c656dce07df05a49523df9304e5e66939b6f33b1
snorret <johan.ung@gmail.com>
2016-10-04 23:14:02 +0200

    First simple socket.io connection

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var express = require('express')
 const config = require('./config.json')
 
+var express = require('express')
+var http = require('http');
+var socketIo = require('socket.io');
+
 var app = express();
+var server = http.Server(app);
+var io = socketIo(server);
 
 app.use(express.static('client'))
 
@@ -9,7 +14,12 @@ app.get('/kug/', function(req, res) {
     res.send('Hello feddaah!');
 });
 
-app.listen(config.port, function() {
+var numberOfConnects = 0;
+io.on('connection', function(socket) {
+    console.log('User connected, number of connects since server started: ' + ++numberOfConnects);
+});
+
+server.listen(config.port, function() {
     console.log('Server running at: ' + config.ip + ':' + config.port);
 });
 
