04229964ce621de4f39e395ac48e08ef33f55a34
snorret <johan.ung@gmail.com>
2016-10-05 22:03:57 +0200

    Added BoomServer and working socket.io example that emits number of connected users

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const config = require('./config.json')
 
+var BoomServer = require('./server/server.js');
+
 var express = require('express')
 var http = require('http');
 var socketIo = require('socket.io');
 
 var app = express();
-var server = http.Server(app);
-var io = socketIo(server);
+var httpServer = http.Server(app);
+var io = socketIo(httpServer);
+var boomServer = new BoomServer(io);
 
 app.use(express.static(__dirname + '/client'))
 
 app.get('/kug/', function(req, res) {
-    res.send('Hello feddaah!');
+    res.send('Hello fedaaadaah!');
 });
 
-var numberOfConnects = 0;
-io.on('connection', function(socket) {
-    console.log('User connected, number of connects since server started: ' + ++numberOfConnects);
-});
-
-server.listen(config.port, function() {
+httpServer.listen(config.port, function() {
     console.log('Server running at: ' + config.ip + ':' + config.port);
 });
 
---
+++ server/server.js
@@ -0,0 +1,39 @@
+function BoomServer(io) {
+    var self = this;
+    self.socketIo = io;
+    self.numberOfCurrentClientsConnected = 0;
+    self.numberOfClientsConnectedSinceStart = 0;
+    
+    self.socketIo.on('connection', function(socket) {
+        self.onClientConnect(socket);
+    });
+}
+
+BoomServer.prototype.onClientConnect = function(socket) {
+    var self =  this;
+    
+    self.numberOfCurrentClientsConnected++;
+    self.numberOfClientsConnectedSinceStart++;
+    
+    socket.broadcast.emit('numberOfClients', self.numberOfCurrentClientsConnected);
+    socket.emit('numberOfClients', self.numberOfCurrentClientsConnected)
+    
+    socket.on('disconnect', function() {
+        self.onClientDisconnect();
+    });
+    
+    console.log('User connected, number of clients since server start: ' + self.numberOfClientsConnectedSinceStart);
+    console.log('User connected now: ' + self.numberOfCurrentClientsConnected);
+}
+
+BoomServer.prototype.onClientDisconnect = function() {
+    var self = this;
+    
+    self.numberOfCurrentClientsConnected--;
+    
+    self.socketIo.emit('numberOfClients', self.numberOfCurrentClientsConnected);
+    
+    console.log('User disconnected, currently connected now: ' + self.numberOfCurrentClientsConnected);
+}
+
+module.exports = BoomServer;
