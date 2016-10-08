a076d45b194108a1da033eaf20d55f1300d57ac6
Darull <Darull>
2016-10-09 01:26:59 +0200

    Test data to server.

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ function BoomServer(io) {
     });
 }
 
+var eventTypes = {
+    setting : "setting",
+    targetData : "targetData"
+}
+
 BoomServer.prototype.onClientConnect = function(socket) {
     var self =  this;
     
@@ -20,6 +25,45 @@ BoomServer.prototype.onClientConnect = function(socket) {
     
     socket.on('disconnect', function() {
         self.onClientDisconnect();
+    });
+    
+    socket.on('clientEvent', function(data) { // Data: "type|value"
+        console.log(data);
+        
+        
+        
+        var dataVal = data.split('|');
+        console.log(dataVal + " - " + dataVal.length);
+        if(dataVal.length != 2) {
+            console.log("data received is not in correct format!");
+            return;
+        }
+        
+        var type = dataVal[0];
+        
+        if(!eventTypes[type]) {
+            console.log("Wrong type received. " + type + " is not defined!");
+            return;
+        }
+        
+        var value = dataVal[1];
+        
+        
+        if(type == eventTypes.targetData) {
+            // radier.
+            var targetValues = value.split('#');
+
+            
+            targetValues.forEach(function(item) {
+                console.log("targetData received: " +item);
+            });
+            
+        }
+        
+        
+
+        socket.emit('testerEvent', { message: 'Received: ' + value });
+        console.log("msg '" + value + "' sent!");
     });
     
     console.log('User connected, number of clients since server start: ' + self.numberOfClientsConnectedSinceStart);
