7d740165a248a80cb9c7460944a308a4f8a0c526
Darull <Darull>
2016-10-16 22:15:34 +0200

    save connected devices. + modal popup

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ function BoomServer(io) {
     self.numberOfCurrentClientsConnected = 0;
     self.numberOfClientsConnectedSinceStart = 0;
     
+    
+    self.connectedDevices = [];
+    
     self.socketIo.on('connection', function(socket) {
         self.onClientConnect(socket);
     });
@@ -11,7 +14,8 @@ function BoomServer(io) {
 
 var eventTypes = {
     setting : "setting",
-    targetData : "targetData"
+    targetData : "targetData",
+    clientInfo : "clientInfo"
 }
 
 BoomServer.prototype.onClientConnect = function(socket) {
@@ -26,6 +30,8 @@ BoomServer.prototype.onClientConnect = function(socket) {
     socket.on('disconnect', function() {
         self.onClientDisconnect();
     });
+    
+    
     
     socket.on('clientEvent', function(data) { // Data: "type|value"
         console.log(data);
@@ -66,8 +72,44 @@ BoomServer.prototype.onClientConnect = function(socket) {
         console.log("msg '" + value + "' sent!");
     });
     
+    
+    
+    socket.on('getClientInfo', function(data) { // Data: "type|value"
+        console.log(data);
+        
+        var dataVal = data.split('|');
+        console.log(dataVal + " - " + dataVal.length);
+        if(dataVal.length != 2) {
+            console.log("data received is not in correct format!");
+            return;
+        }
+        
+        var type = dataVal[0];
+        
+        if(!eventTypes[type]) {
+            console.log("Wrong type received. " + type + " is not defined!");
+            return;
+        }
+        var value = dataVal[1];
+        
+        
+        
+        
+        if(type == eventTypes.clientInfo) {
+            socket.emit("clientInfoEvent", { type: eventTypes.clientInfo, address: socket.handshake.address, connectedClients: self.connectedDevices, date: value });
+            console.log("msg '" + value + "' sent!");
+        }
+    });
+    
     console.log('User connected, number of clients since server start: ' + self.numberOfClientsConnectedSinceStart);
     console.log('User connected now: ' + self.numberOfCurrentClientsConnected);
+    
+    
+    console.log('Test!')
+    
+    self.connectedDevices[self.connectedDevices.length] = socket.handshake.address;
+    console.log(socket.handshake.address);
+    
 }
 
 BoomServer.prototype.onClientDisconnect = function() {
